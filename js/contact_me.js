24f86ab65eef73990cb5645fb77b0038fb6b54da
tanomartin <tanomartin05@gmail.com>
2017-08-07 14:41:49 -0300

    subida de titulos

--- js/contact_me.js
+++ js/contact_me.js
@@ -40,7 +40,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Su Mensaje ha sido enviado satisfactorioamente. </strong>");
+                        .append("<strong>Su Mensaje ha sido enviado satisfactoriamente. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
